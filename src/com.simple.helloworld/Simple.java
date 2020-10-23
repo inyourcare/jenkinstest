1acc60135927f334d780d4ffdccb9056447a14ca
kihoon kwon <kihoon.kwon@smoothy.co>
2020-10-23 18:45:31 +0900

    jenkinstest#1

--- src/com.simple.helloworld/Simple.java
+++ src/com.simple.helloworld/Simple.java
@@ -2,6 +2,6 @@ package com.simple.helloworld;
 
 public class Simple{
     public static void main(String args[]){
-        System.out.println("Hello world");
+        System.out.println("Hello world!!");
     }
 }
