068f783866c41f850f34120692606d706de23f2d
kihoon kwon <kihoon.kwon@smoothy.co>
2020-12-07 17:41:47 +0900

    jenkins test3!

--- src/com.simple.helloworld/Simple.java
+++ src/com.simple.helloworld/Simple.java
@@ -2,6 +2,6 @@ package com.simple.helloworld;
 
 public class Simple{
     public static void main(String args[]){
-        System.out.println("Hello world2!!");
+        System.out.println("Hello world3!!");
     }
 }
